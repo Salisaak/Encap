808cca26cd3d3b9431cafe8310cca8818640ba68
Saahir <Salisaak@gmail.com>
2021-09-21 12:53:09 -0400

    Add files via upload

---
+++ LearningEncap.java
@@ -0,0 +1,37 @@
+package Encap;
+
+public class LearningEncap {
+        private String user;
+        private String idNum;
+        private int age;
+
+        public int getAge() {
+            return age;
+        }
+        public String getUser() {
+            return user;
+        }
+        public String getIdNum() {
+            return idNum;
+        }
+        public void setAge( int newAge) {
+            age = newAge;
+        }
+        public void setName(String userName) {
+            user = userName;
+        }
+        public void setIdNum( String newId) {
+            idNum = newId;
+        }
+    public static void main(String args[]) {
+        LearningEncap encap = new LearningEncap();
+        encap.setName("Yjol D.");
+        encap.setAge(69);
+        encap.setIdNum("173842");
+
+        System.out.print("Name : " + encap.getUser() + " Age : " + encap.getAge() + " ID : "+ encap.getIdNum());
+    }
+
+
+    }
+
